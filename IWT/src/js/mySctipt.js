2f9bf67081f0e684f14217e04ce48773ca1a8792
Pramod Lakmal <pramodlakmalofficial@gmail.com>
2023-04-02 12:05:39 -0700

    completed the about.php and fixed some linking errors

--- IWT/src/js/mySctipt.js
+++ IWT/src/js/mySctipt.js
@@ -1,27 +1,28 @@
 function loadData(data) {
     if (data === 'product1') {
-        document.getElementById("image").src = "/IWT/src/images/XS.jpg";
+        // I can't link the images on the images folder. So i used external images. I was unable to find a way to fix that error.
+        document.getElementById("image").src = "https://fdn2.gsmarena.com/vv/pics/apple/apple-iphone-xs-max-5.jpg"; 
         document.getElementById("image").style.display ='block';
         document.getElementById("text").innerHTML = "<span>Iphone XS</span> :- Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita blanditiis sit fugiat molestias voluptatem nostrum. Eaque mollitia, non, accusantium qui, illum sapiente quis molestias necessitatibus cumque labore aut tempore at.";
         document.getElementById("text").style.display ='block';
         document.getElementById("text1").style.display ='none';
     }
     else if (data === 'product2') {
-        document.getElementById("image").src = "/IWT/src/images/XSMax.jpg";
+        document.getElementById("image").src = "https://i0.wp.com/mobilereborn.co.uk/wp-content/uploads/2022/06/iPhone_XS_silver.png?fit=1200%2C1200&ssl=1";
         document.getElementById("image").style.display ='block';
         document.getElementById("text").innerHTML = "<span>Iphone XS Max</span> :- Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita blanditiis sit fugiat molestias voluptatem nostrum. Eaque mollitia, non, accusantium qui, illum sapiente quis molestias necessitatibus cumque labore aut tempore at.";
         document.getElementById("text").style.display ='block';
         document.getElementById("text1").style.display ='none';
     }
     else if (data === 'product3') {
-        document.getElementById("image").src = "/IWT/src/images/8plus.jpg";
+        document.getElementById("image").src = "https://cdn.shopify.com/s/files/1/0506/5421/6368/products/Gold_96983daa-2aa6-42ff-96f3-3b967e482ca7_600x.png?v=1679666054";
         document.getElementById("image").style.display ='block';
         document.getElementById("text").innerHTML = "<span>Iphone 8 Plus</span> :- Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita blanditiis sit fugiat molestias voluptatem nostrum. Eaque mollitia, non, accusantium qui, illum sapiente quis molestias necessitatibus cumque labore aut tempore at.";
         document.getElementById("text").style.display ='block';
         document.getElementById("text1").style.display ='none';
     }
     else if (data === 'product4') {
-        document.getElementById("image").src = "/IWT/src/images/SE.png";
+        document.getElementById("image").src = "https://fdn.gsmarena.com/imgroot/news/20/04/apple-iphone-se-official/-727w3/gsmarena_002.jpg";
         document.getElementById("image").style.display ='block';
         document.getElementById("text").innerHTML = "<span>Iphone SE</span> :- Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita blanditiis sit fugiat molestias voluptatem nostrum. Eaque mollitia, non, accusantium qui, illum sapiente quis molestias necessitatibus cumque labore aut tempore at.";
         document.getElementById("text").style.display ='block';
